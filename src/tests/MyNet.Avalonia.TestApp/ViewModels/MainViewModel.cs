09da6085642b80982886fa43ed375520d16b58c1
Stéphane ANDRE <andre.cs2i@gmail.com>
2024-12-16 19:54:30 +0100

    fix ci

--- src/tests/MyNet.Avalonia.TestApp/ViewModels/MainViewModel.cs
+++ src/tests/MyNet.Avalonia.TestApp/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace MyNet.Avalonia.TestApp.ViewModels;
+﻿// Copyright (c) Stéphane ANDRE. All Right Reserved.
+// See the LICENSE file in the project root for more information.
+
+namespace MyNet.Avalonia.TestApp.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
